c7ef137089ba6fc87beba9017da467c2bf207d9d
Lani <lani.mortontran@movemedical.com>
2022-07-15 14:22:46 -0700

    List of Transaction Cards stub

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,17 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: [
+                    Text(tx.amount.toString()),
+                    Column(
+                      children: [
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    ),
+                  ],
+                ),
               );
             }).toList(),
           ),
