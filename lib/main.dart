2b9c9e31595c9ed5e052cc45f909dc47c2157c44
Lani <lani.mortontran@movemedical.com>
2022-07-17 15:58:17 -0700

    ShowModalBottomSheet
    Lift state up in order to display modal and pass in addNewTransaction method.
    No longer need UserTransaction widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/new_transaction.dart';
 
-import 'widgets/user_transactions.dart';
+import 'models/transaction.dart';
+import 'widgets/transaction_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,7 +16,104 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'New Shoes',
+      amount: 12,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Weekly Groceries',
+      amount: 15.0,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'New Shoes',
+      amount: 75.43,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Weekly Groceries',
+      amount: 107.09,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'New Shoes',
+      amount: 33.89,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't8',
+      title: 'Weekly Groceries',
+      amount: 1.07,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't9',
+      title: 'New Shoes',
+      amount: 5.93,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't10',
+      title: 'Weekly Groceries',
+      amount: 254.39,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(), // Just for purposes of this app, this way of generating an id is fine
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      // This is a final variable, so the address of the object stored cannot be changed.
+      // However, the list that the object holds can be manipulated because it will not generate a new pointer.
+      // It is simply modifying the data that the object stores.
+      _userTransaction.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(onAddHandler: _addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +121,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
         actions: <Widget>[
           IconButton(
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(context),
             icon: Icon(Icons.add),
           ),
         ],
@@ -40,12 +139,12 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5.0,
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransaction),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(onPressed: () => _startAddNewTransaction(context), child: Icon(Icons.add)),
     );
   }
 }
