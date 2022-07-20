0edb27953d52cfcacce58207a4683e6848143507
Lani <lani.mortontran@movemedical.com>
2022-07-20 13:23:41 -0700

    DatePicker

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function onAddHandler;
@@ -10,14 +11,14 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime _selectedDate;
 
-  final amountController = TextEditingController();
-
-  void submitData() {
+  void _submitData() {
     try {
-      final enteredTitle = titleController.text;
-      final enteredAmount = double.parse(amountController.text);
+      final enteredTitle = _titleController.text;
+      final enteredAmount = double.parse(_amountController.text);
 
       if (enteredTitle.isEmpty || enteredAmount <= 0) {
         return;
@@ -29,6 +30,23 @@ class _NewTransactionState extends State<NewTransaction> {
     } catch (e) {
       return;
     }
+  }
+
+  void _displayDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -44,18 +62,36 @@ class _NewTransactionState extends State<NewTransaction> {
               // onChanged: (value) {
               //   titleInput = value;
               // },
-              controller: titleController,
-              onSubmitted: (_) => submitData(), // Triggered when green check on keyboard is pressed
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(), // Triggered when green check on keyboard is pressed
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               // onChanged: (value) => amountInput = value,
-              controller: amountController,
+              controller: _amountController,
               keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => submitData(), // Triggered when green check on keyboard is pressed
+              onSubmitted: (_) => _submitData(), // Triggered when green check on keyboard is pressed
+            ),
+            Container(
+              height: 70.0,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(_selectedDate == null ? 'No Date Chosen!' : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}'),
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    onPressed: _displayDatePicker,
+                    child: Text(
+                      'Choose Date',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
             ),
             FlatButton(
-              onPressed: submitData,
+              onPressed: _submitData,
               child: Text('Add Transaction'),
               color: Theme.of(context).primaryColor,
               // textColor: Theme.of(context).textTheme.labelSmall.color,
