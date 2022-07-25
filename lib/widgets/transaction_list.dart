1f938ef147ca9f7e665c9f72ade213169db6eafc
Lani <lani.mortontran@movemedical.com>
2022-07-25 12:27:27 -0700

    Extract Transaction Item widget

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import 'transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> _transactions;
@@ -74,45 +74,7 @@ class TransactionList extends StatelessWidget {
               //     ],
               //   ),
               // );
-              return Card(
-                margin: EdgeInsets.symmetric(
-                  vertical: 8.0,
-                  horizontal: 5.0,
-                ),
-                elevation: 5.0,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30.0,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: FittedBox(
-                        child: Text('\$${_transactions[index].amount}'),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    _transactions[index].title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(_transactions[index].date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 450
-                      ? FlatButton.icon(
-                          onPressed: () => deleteTx(_transactions[index].id),
-                          icon: Icon(Icons.delete),
-                          label: Text('Delete'),
-                          textColor: Theme.of(context).errorColor,
-                        )
-                      : IconButton(
-                          icon: Icon(
-                            Icons.delete,
-                            color: Theme.of(context).errorColor,
-                          ),
-                          onPressed: () => deleteTx(_transactions[index].id),
-                        ),
-                ),
-              );
+              return TransactionItem(transaction: _transactions[index], deleteTx: deleteTx);
             },
             itemCount: _transactions.length,
           );
